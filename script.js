bde7be294f52d763555c06ed6067226cf44800cc
𝔬𝔵𝔩 <egodominus0x1@gmail.com>
2025-09-12 07:43:58 +0330

    Update script.js

--- script.js
+++ script.js
@@ -58,29 +58,38 @@ let frameIndex = 0;
 let progress = 0;
 let typing = true;
 
-let interval = setInterval(() => {
-  if (typing) {
-    loadingText.textContent = frames[frameIndex];
-    frameIndex++;
+// let interval = setInterval(() => {
+//   if (typing) {
+//     loadingText.textContent = frames[frameIndex];
+//     frameIndex++;
 
-    if (frameIndex >= frames.length) {
-      typing = false;
-      loadingText.textContent = "> Loading";
-      dots.style.display = "inline"; // فعال شدن انیمیشن نقطه‌ها
-    }
-  }
+//     if (frameIndex >= frames.length) {
+//       typing = false;
+//       loadingText.textContent = "> Loading";
+//       dots.style.display = "inline";
+//     }
+//   }
 
-  // پیشرفت بار
-  if (progress < 100) {
-    progress += 2;
-    progressBar.style.width = progress + "%";
-    progressText.textContent = progress + "%";
-  } else {
-    clearInterval(interval);
-    // وقتی کامل شد
-    loadingText.textContent = "✓ Completed!";
-    loadingText.classList.add("success");
-    dots.style.display = "none";
-    progressContainer.style.display = "none"; // پراگرس بار حذف بشه
-  }
-}, 200);
+//   // پیشرفت بار
+//   if (progress < 100) {
+//     progress += 2;
+//     progressBar.style.width = progress + "%";
+//     progressText.textContent = progress + "%";
+//   } else {
+//     clearInterval(interval);
+//     // وقتی کامل شد
+//     loadingText.textContent = "✓ Completed!";
+//     loadingText.classList.add("success");
+//     dots.style.display = "none";
+//     progressContainer.style.display = "none"; // پراگرس بار حذف بشه
+//   }
+// }, 200);
+
+function loadingWord(word) {
+  const frames = [];
+  for (let i = 0; i < 10; i++) frames.push(`> ${i % 2 ? '_' : ''}`);
+
+  return frames;
+}
+
+const wordsFrames = loadingWord("Loading");
